5185527d853f6d1a2c4963fd71fa953ed3df797f
Rahul Yadav <rahulyadav.cs1@gmail.com>
2026-02-13 23:00:17 +0530

    update code lines

--- config/urls.py
+++ config/urls.py
@@ -28,24 +28,15 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='home'),  # homepage
     # path('', include('store.urls')),
-
     path('products/', include('apps.products.urls')),
     path('accounts/', include('apps.accounts.urls')),  # ❌ No namespace
-
     path('admin_panel/', include('apps.admin_panel.urls')),  # include your admin_panel URLs
-    
     path('cart/', include('apps.cart.urls')),
     path('wishlist/', include('apps.wishlist.urls')),
     path('orders/', include('apps.orders.urls')),
     path('pages/', include('apps.core.urls')),  # Footer pages
-# Custom admin login at project root
+    # Custom admin login at project root
     path('admin-login/', admin_views.admin_login, name='admin_login'),
-
-
-    
-
-
-
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
