53227c557ca4cfebd4411e76a16c335b3af701c9
Rahul Yadav <rahulyadav.cs1@gmail.com>
2026-02-14 07:47:13 +0530

    commit

--- apps/core/page_views.py
+++ apps/core/page_views.py
@@ -1,19 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import TemplateView
+from apps.products.models import Product, Category
+from apps.store.models import HomeSlider
 
-def about_us(request):
-    return render(request, 'user_theme/pages/about.html')
 
-def payment_policy(request):
-    return render(request, 'user_theme/pages/payment_policy.html')
+class HomeView(TemplateView):
+    template_name = 'user_theme/pages/home.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['sliders'] = HomeSlider.objects.filter(is_active=True)
+        context['categories'] = Category.objects.filter(is_active=True)[:6]
+        context['featured_products'] = Product.objects.filter(is_available=True)[:8]
+        context['best_selling_products'] = Product.objects.filter(is_available=True)[:8]
+        return context
 
-def terms_conditions(request):
-    return render(request, 'user_theme/pages/terms_conditions.html')
 
-def return_refund(request):
-    return render(request, 'user_theme/pages/return_refund.html')
+class CategoryView(TemplateView):
+    template_name = 'user_theme/pages/category.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        slug = self.kwargs.get('slug')
+        
+        if slug:
+            category = get_object_or_404(Category, slug=slug, is_active=True)
+            products = Product.objects.filter(category=category, is_available=True)
+        else:
+            category = None
+            products = Product.objects.filter(is_available=True)
+            
+        context['category'] = category
+        context['products'] = products
+        context['categories'] = Category.objects.filter(is_active=True)
+        return context
 
-def shipping_policy(request):
-    return render(request, 'user_theme/pages/shipping_policy.html')
 
-def warranty(request):
-    return render(request, 'user_theme/pages/warranty.html')
+class ProductDetailView(TemplateView):
+    template_name = 'user_theme/pages/product_detail.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        slug = self.kwargs.get('slug')
+        product = get_object_or_404(Product, slug=slug, is_available=True)
+        
+        context['product'] = product
+        context['related_products'] = Product.objects.filter(
+            category=product.category, 
+            is_available=True
+        ).exclude(id=product.id)[:6]
+        return context
--- apps/core/urls.py
+++ apps/core/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from apps.core import page_views
 
 urlpatterns = [
+    path('', page_views.HomeView.as_view(), name='home'),
+    path('category/', page_views.CategoryView.as_view(), name='category_all'),
+    path('category/<slug:slug>/', page_views.CategoryView.as_view(), name='category'),
+    path('product/<slug:slug>/', page_views.ProductDetailView.as_view(), name='product_detail'),
     path('about-us/', page_views.about_us, name='about_us'),
     path('payment-policy/', page_views.payment_policy, name='payment_policy'),
     path('terms-conditions/', page_views.terms_conditions, name='terms_conditions'),
